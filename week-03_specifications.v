6cec517a57ee1a81dc0e1a5f6cd324b6d4fb3996
zhengqunkoo <root@zhengqunkoo.com>
2020-09-05 22:57:06 +0800

    Add comments

--- week-03_specifications.v
+++ week-03_specifications.v
@@ -287,77 +287,6 @@ Qed.
 
 (* Exercise 5 *)
 
-(* Since the resident addition function satisfies both specifications, it is
-reasonable to believe that the specifications are equivalent. To prove this, we
-need to show that the definitions have the same "expressive power", which means we
-can take the assertions from either and rewrite them to obtain the assertions in
-the other. *)
-Theorem the_two_specifications_of_addition_are_equivalent :
-  forall add : nat -> nat -> nat,
-    recursive_specification_of_addition add <-> tail_recursive_specification_of_addition add.
-Proof.
-  intro add.
-  unfold recursive_specification_of_addition.
-  unfold tail_recursive_specification_of_addition.
-  (* In the forward direction, we will prove that the recursive specification
-  can be used to construct the tail recursive specification. *)
-  split.
-  - intros [H_add_O H_add_S].
-    split.
-    -- exact H_add_O.
-    -- intros x' y.
-       rewrite -> (H_add_S x' y).
-       Search (S (_ + _) = _ + (S _)).
-       (* Rewritten this way, the current subgoal matches the lemma [plus_n_Sm]
-       which exists for the resident addition function.*)
-       assert (H: forall n m : nat, S (add n m) = add n (S m)).
-       * induction n as [| n' IH].
-         ** intro m.
-            rewrite -> (H_add_O m).
-            rewrite -> (H_add_O (S m)).
-            reflexivity.
-         ** intro m.
-            rewrite -> (H_add_S n' m).
-            rewrite -> (IH m).
-            rewrite <- (H_add_S n' (S m)).
-            reflexivity.
-       * exact (H x' y).
-  (* In the backwards direction, we will prove that the tail recursive
-  specification can be used to construct the recursive specification. *)
-  - intros [H_add_O H_add_S_tr].
-    split.
-    -- exact H_add_O.
-    -- intros x' y.
-       rewrite -> (H_add_S_tr x' y).
-       symmetry.
-       (* By flipping the equation, we find the same lemma which was proven
-       earlier. However, we cannot reuse the proof verbatim as we have a
-       different set of assumptions this time. *)
-       assert (H: forall n m : nat, S (add n m) = add n (S m)).
-       * induction n as [| n' IH].
-         ** intro m.
-            rewrite -> (H_add_O m).
-            rewrite -> (H_add_O (S m)).
-            reflexivity.
-            (* The symmetry is an illusion. There does not seem to be a way to
-            prove the inductive step using simple rewrites. Instead, we will
-            proceed with induction over the other variable. *)
-         ** induction m as [| m' IH'].
-            *** rewrite -> (H_add_S_tr n' 0).
-                rewrite -> (IH 1).
-                rewrite -> (H_add_S_tr n' 1).
-                reflexivity.
-            *** (* LHS *)
-                rewrite <- IH'.
-                rewrite -> (H_add_S_tr n' m').
-                (* RHS *)
-                rewrite -> (H_add_S_tr n' (S (S m'))).
-                rewrite <- (IH (S (S m'))).
-                rewrite <- (IH (S m')).
-                reflexivity.
-       * exact (H x' y).
-Qed.
-
 (** This alternative proof exploits the fact that there is at most one function that satisfies [recursive_specification_of_addition] (ditto for [tail_recursive_specification_of_addition]). Since the resident addition [Nat.add] satisfies both specifications, we're left with proving the resident addition is equivalent to itself.
 
  First, we prove that all functions satisfying both specifications are equivalent to [Nat.add] through proof by cases:
@@ -426,9 +355,47 @@ Proof.
       exact (plus_Sn_m x' y).
 Qed.
 
-(* TODO: Remarks *)
+(**
+Since the resident addition function [Nat.add] satisfies both specifications, it is
+reasonable to believe that the specifications are equivalent
+(though we do not know for sure until we prove it).
+To prove this, we
+need to show that the definitions have the same "expressive power", which means we
+can take the assertions from either and rewrite them to obtain the assertions in
+the other.
 
-Theorem the_two_specifications_of_addition_are_equivalent'' :
+We eyeball the two specifications of addition, and see some similarities.
+
+Looking at [recursive_specification_of_addition], let [H_recursive_specification_of_addition_O] be
+[[
+  (forall y : nat,
+      add O y = y)
+]]
+and let [H_recursive_specification_of_addition_S] be
+[[
+  (forall x' y : nat,
+      add (S x') y = S (add x' y)).
+]]
+
+Looking at [tail_recursive_specification_of_addition], let [H_tail_recursive_specification_of_addition_O] be
+[[
+  (forall y : nat,
+      add O y = y)
+]]
+and let [H_tail_recursive_specification_of_addition_S] be
+[[
+  (forall x' y : nat,
+      add (S x') y = add x' (S y)).
+]]
+
+Then we observe:
+- The first conjucts of the two specifications are syntactically equivalent. This results in a straightforward use of the [exact] tactic when proving the first conjuncts in both directions of implication.
+- The second conjucts of the two specifications are very similar. This suggests the proofs of the second conjuncts are very similar in both directions of implication: only the right-hand side of the equation differs. To be exact, we expect the proofs to be almost identical, differing only in where the successor function [S] is used: in [H_recursive_specification_of_addition_S], the [S] is applied to the output of a call of [add] on a structurally decreasing [x'], whereas in [H_tail_recursive_specification_of_addition_S], the successor function [S] is applied to the accumulator argument [y], with the result of application being passed forward into the call of [add] on a structurally decreasing [x']. In other words, we expect the difference in proof to somewhat correspond to the difference in specification.
+
+In addition, the observation of the second conjuncts of the two specifications establishes that our proof of these two conjucts must use induction on [x']. We shall see that induction is actually used here in our proof.
+
+ *)
+Theorem the_two_specifications_of_addition_are_equivalent :
   forall add : nat -> nat -> nat,
     recursive_specification_of_addition add <-> tail_recursive_specification_of_addition add.
 Proof.
@@ -436,29 +403,112 @@ Proof.
   unfold recursive_specification_of_addition.
   unfold tail_recursive_specification_of_addition.
   split.
+  (**
+     In proving implication in the forward direction, we will prove that the recursive specification can be used to construct the tail recursive specification.
+
+     First, we introduce the hypotheses, and split on the cases of induction.
+
+   *)
   - intros [H_recursive_specification_of_addition_O H_recursive_specification_of_addition_S].
     split.
+    (**
+       As noted earlier, the first conjunct of the recursive specification is exactly the first conjunct of the tail recursive specification.
+
+     *)
     + exact H_recursive_specification_of_addition_O.
+    (**
+       The induction hypothesis [IHx''] here is necessary, since in order to prove the second conjunct, we must assume the truth of the hypothesis for structurally smaller inputs.
+
+     *)
     + induction x' as [ | x'' IHx''].
       * intros y.
+        (**
+           Because we want to prove the goal [add 1 y = add 0 (S y)], we are forced to rewrite the left-hand side of the goal with [H_recursive_specification_of_addition_S], as this is the only hypothesis with a non-[0] value in the first input of [add].
+
+           This rewriting corresponds to one small-step of computation, where [add 1 y] is reduced to [S (add 0 y)].
+
+         *)
         rewrite -> (H_recursive_specification_of_addition_S O y).
+
+        (**
+           To terminate the aforementioned computation, we need to perform a [rewrite] that corresponds to evaluating the left-hand side of the goal [S (add 0 y)] into a term that does not contain [add]. Then, the rewritten term will belong to [nat], in the sense that it can be constructed from just the constructors of [nat].
+
+         *)
         rewrite -> (H_recursive_specification_of_addition_O y).
+
+        (**
+           The computation on the right-hand side of the goal is similarly terminated, by performing a [rewrite] that corresponds to evaluating the left-hand side of the goal [add 0 (S y)] into a term that does not contain [add]. Then, the rewritten term will belong to [nat], in the sense that it can be constructed from just the constructors of [nat].
+
+         *)
         rewrite -> (H_recursive_specification_of_addition_O (S y)).
+
+        (**
+           Then, because the terms on both sides of the equation in the goal are constructed from just the constructers in [nat], the truth of the goal can be established by [reflexivity].
+
+         *)
         reflexivity.
       * intros y.
+        (**
+           Because we cannot reasonably use [H_recursive_specification_of_addition_O], we rewrite with [H_recursive_specification_of_addition_S].
+
+           Because the inductive hypothesis
+           [[
+           IHx'' : forall y : nat, add (S x'') y = add x'' (S y)
+           ]]
+           is available for our use, we are motivated to rewrite the goal so that the goal "matches" [IHx''], in the sense that [IHx''] has the form [x = y], and the goal has the form [P(x) = P(y)], so that rewriting the goal with [IHx''] changes the goal to [P(y) = P(y)].
+
+           This is exactly what we do. Concretely, we note that the goal is [add (S (S x'')) y = add (S x'') (S y)], where on both sides of the goal, the first input to [add] has one more occurence of [S] than needed to apply [IHx'']. So, we need to rewrite both sides of the goal with [H_recursive_specification_of_addition_S] from left to right. This moves [S] from the first input to [add], to be applied to the output of [add], thus decreasing the number of occurences of [S] on the first input to [add] by one, on both sides of the goal. Then, we are allowed to rewrite the goal with [IHx''].
+
+           Then, we are allowed conclude proof of the case by [reflexivity].
+
+         *)
         rewrite -> (H_recursive_specification_of_addition_S x'' (S y)).
         rewrite -> (H_recursive_specification_of_addition_S (S x'') y).
         rewrite -> (IHx'' y).
         reflexivity.
+  (**
+     In proving implication in the backwards direction, we will prove that the tail recursive specification can be used to construct the recursive specification.
+
+   *)
   - intros [H_tail_recursive_specification_of_addition_O H_tail_recursive_specification_of_addition_S].
     split.
+    (**
+       As noted earlier, the first conjunct of the recursive specification is exactly the first conjunct of the tail recursive specification.
+
+     *)
     + exact H_tail_recursive_specification_of_addition_O.
+    (**
+       As noted earlier, the proof of the second conjunct is mostly similar to the proof for the second conjunct of implication in the forwards direction.
+
+     *)
     + induction x' as [ | x'' IHx''].
+      (**
+         For the base case of induction, the proof is almost identical to the corresponding proof of base case of induction for the second conjunct for implication in the forwards direction.
+
+         For the first rewrite, the left-hand side of the goal [add 1 y] is identical to the left-hand side of the goal of the corresponding proof, **and** the left-hand side of the hypothesis [H_tail_recursive_specification_of_addition_S] here is the same as the left-hand side of the hypothesis before [H_recursive_specification_of_addition_S], therefore we instantiate [H_tail_recursive_specification_of_addition_S] here with the same inputs as the inputs given in the first rewrite of the corresponding proof.
+
+         Now the goal is [add 0 (S y) = S (add 0 y)], which is just the previous goal [S (add 0 y) = add 0 (S y)] but with the left-hand side and right-hand side switched.
+
+         So for the second and third rewrites, as before, we proceed to remove [add] from our goal by rewriting the terms into a term of [nat], then conclude with [reflexivity].
+
+         The technical reason for this switching of left-hand side and right-hand side is, the recursive specification and the tail recursive specification differ only in the right-hand side of their second conjuncts.
+         - In proving implication in the forward direction, the second conjunct of the recursive specification reduces the left-hand side of the goal to [S (add 0 y)], while the right-hand side of the goal is the right-hand side of the second conjunct of the tail recursive specification, instantiated at the base case [x' = 0], so the right-hand side of the goal is [add 0 (S y)]. The first conjunct of the recursive specification is enough to reduce both sides of the goal to the term [S y].
+         - The proof for implication in the backward direction is similar.
+
+       *)
       * intros y.
         rewrite -> (H_tail_recursive_specification_of_addition_S O y).
         rewrite -> (H_tail_recursive_specification_of_addition_O y).
         rewrite -> (H_tail_recursive_specification_of_addition_O (S y)).
         reflexivity.
+      (**
+         For the inductive case, as noted earlier, the proof is very similar to the proof of inductive case for the forward direction of implication, with the only difference being the location of the successor function [S].
+
+         As in the corresponding case in the proof of implication in the forward direction, the input to [add] has one more [S] than what would be possible to rewrite the goal with [IHx''] with. So, we need to rewrite with [H_tail_recursive_specification_of_addition_S] on both sides of the goal. Here is where the proof diverges with the corresponding proof from before.
+
+         The rewrites move [S] from the first input of [add] to the second input of [add]. Therefore, we need to instantiate [IHx''] with [S y] instead of [y] as in the corresponding proof from before.
+
+       *)
       * intros y.
         rewrite -> (H_tail_recursive_specification_of_addition_S x'' y).
         rewrite -> (H_tail_recursive_specification_of_addition_S (S x'') y).
